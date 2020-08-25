69725989eee13b6efea9c2c859030875b665dbfb
Kevin Zou <kevinzou2150@gmail.com>
2020-08-25 18:29:53 -0400

    PySimpleGUI env setup

---
+++ main.py
@@ -0,0 +1,26 @@
+import PySimpleGUI as sg
+
+sg.theme('Default 1')
+
+menu_def = [['&File', ['&Open', '&Save', 'E&xit', 'Properties']],
+            ['&Edit', ['Paste', ['Special', 'Normal', ], 'Undo'], ],
+            ['&Help', '&About...'], ]
+
+layout = [[sg.Menu(menu_def, tearoff=False)],
+          [sg.Text('Your typed chars appear here:'),
+           sg.Text(size=(12, 1), key='-OUTPUT-')],
+          [sg.Input(key='-IN-')],
+          [sg.Button('Show'), sg.Button('Exit')]]
+
+window = sg.Window('Window Title', layout)
+
+while True:  # Event Loop
+    event, values = window.read()
+    print(event, values)
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    if event == 'Show':
+        # change the "output" element to be the value of "input" element
+        window['-OUTPUT-'].update(values['-IN-'])
+
+window.close()
