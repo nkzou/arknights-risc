2a7b7b4c2b4105177e0e0ff5216bf84d84008b55
Kevin Zou <kevinzou2150@gmail.com>
2020-08-28 17:30:19 -0400

    add physical and arts damage columns, reorder frame and layout code

--- main.py
+++ main.py
@@ -7,47 +7,52 @@ menu_def = [
     ['&Help', '&About...']
 ]
 
-atk_frame = sg.Frame('Attacker', [
+atk_frame = sg.Frame('Source Stats', [
     [sg.Text("Base Attack", font="Any 12")],
     [sg.InputText(size=(20, None), font="Any 12")]
 ], font="Any 14")
 
-spd_frame = sg.Frame('Attack Speed', [
+spd_frame = sg.Frame('Source Attack Speed', [
     [sg.Text("Base Interval", font="Any 12")],
     [sg.InputText(size=(20, None), font="Any 12")]
 ], font="Any 14")
 
-def_frame = sg.Frame('Defender', [
+def_frame = sg.Frame('Target Stats', [
     [sg.Text("Base Defense", font="Any 12")],
     [sg.InputText(size=(20, None), font="Any 12")]
 ], font="Any 14")
 
-mov_frame = sg.Frame('Defender Movement', [
+mov_frame = sg.Frame('Target Walk Speed', [
     [sg.Text("Movement Speed", font="Any 12")],
     [sg.InputText(size=(20, None), font="Any 12")]
 ], font="Any 14")
 
-input_layout = [
-    sg.Column([[atk_frame]], expand_y=True),
-    sg.Column([[spd_frame]], expand_y=True),
-    sg.Column([[def_frame]], expand_y=True),
-    sg.Column([[mov_frame]], expand_y=True)
-]
+phy_frame = sg.Frame('Physical Damage', [
+    [sg.Text("Attack Scale", font="Any 12"),
+     sg.InputText(size=(20, None), font="Any 12")]
+], font="Any 14")
+
+art_frame = sg.Frame('Arts Damage', [
+    [sg.Text("Attack Scale", font="Any 12"),
+     sg.InputText(size=(20, None), font="Any 12")]
+], font="Any 14")
 
 dps_frame = sg.Frame("Damage Breakdown", [[
 
 ]], font="Any 14")
 
-output_layout = [
-    sg.Column([[dps_frame]], expand_x=True, expand_y=True)
-]
-
 layout = [
-    input_layout,
-    output_layout
+    [sg.Column([[atk_frame]], expand_y=True),
+     sg.Column([[spd_frame]], expand_y=True),
+     sg.Column([[def_frame]], expand_y=True),
+     sg.Column([[mov_frame]], expand_y=True)],
+    [sg.Column([[phy_frame]], expand_x=True, expand_y=True),
+     sg.Column([[art_frame]], expand_x=True, expand_y=True)],
+    [sg.Column([[dps_frame]], expand_x=True, expand_y=True)]
 ]
 
-frames = [atk_frame, spd_frame, def_frame, mov_frame, dps_frame]
+frames = [atk_frame, spd_frame, def_frame,
+          mov_frame, dps_frame, phy_frame, art_frame]
 
 window = sg.Window('Rhodes Island Simulation Complex', layout, finalize=True)
 
